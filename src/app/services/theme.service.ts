d4a2871292ee4900ca30b7e991d4fbde684b112e
Abdulrhman <abdonaserpoly464@gmail.com>
2025-04-24 15:23:49 +0200

    The Start of The App
    1-Add The Main Libraries and Fonts.
    2- Make The Header component.
    3- Make the Theme Service.
    4-UpNext Add The Theme Feature

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private readonly THEME_KEY = 'Quiz-App-Theme';
+  private isDarkTheme = false;
+
+  constructor() {
+    this.initTheme();
+  }
+
+  initTheme() {
+    const theme = localStorage.getItem(this.THEME_KEY);
+    if (theme) {
+      this.isDarkTheme = theme === 'dark';
+    } else {
+      localStorage.setItem(this.THEME_KEY, this.isDarkTheme ? 'dark' : 'light'); // Fallback to System Preference
+      this.isDarkTheme = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches;
+    }
+    this.applyTheme();
+  }
+
+  applyTheme() {
+    if (this.isDarkTheme) {
+      document.documentElement.classList.add('dark-theme');
+    } else {
+      document.documentElement.classList.remove('dark-theme');
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    localStorage.setItem(this.THEME_KEY, this.isDarkTheme ? 'dark' : 'light');
+    this.applyTheme();
+  }
+}
